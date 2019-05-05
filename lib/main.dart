a7ba0944f73c47ccf02539167148a5f1cb8dc338
Kai Ouchi <ko.rikujyou1114@gmail.com>
2019-05-05 21:12:20 +0900

    Day7 enhanced Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import './pages/input_attendance_hours_page.dart';
 
-// void main() => runApp(MaterialApp(
-//   theme: ThemeData.light(),
-//   home: InputAttendanceHoursPage(),
+void main() => runApp(new MyApp());
 
-// ));
-void main() => runApp(new MyApp()); 
-
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -19,12 +14,12 @@ class MyApp extends StatelessWidget{
   }
 }
 
-class RootPage extends StatefulWidget{
+class RootPage extends StatefulWidget {
   @override
   _RootPageState createState() => new _RootPageState();
 }
 
-class _RootPageState extends State<RootPage>{
+class _RootPageState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,15 +27,86 @@ class _RootPageState extends State<RootPage>{
         title: Text('勤怠管理アプリ'),
       ),
       body: InputAttendanceHoursPage(),
-      drawer: Drawer(
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            Icon(Icons.android),
-            Icon(Icons.android),
-          ],
-        ),
-      ),
+      drawer: _getMailAccountDrwer(),
     );
   }
+}
+Drawer _getMailAccountDrwer(){
+  var drawerIcons = [
+    Icon(Icons.work),
+    Icon(Icons.inbox),
+    Icon(Icons.people),
+    Icon(Icons.local_offer),
+    Icon(Icons.star),
+    Icon(Icons.access_time),
+    Icon(Icons.label),
+    Icon(Icons.send),
+    Icon(Icons.send),
+    Icon(Icons.note),
+    Icon(Icons.mail),
+    Icon(Icons.error),
+    Icon(Icons.delete),
+    Icon(Icons.label),
+    Icon(Icons.label),
+    Icon(Icons.settings),
+    Icon(Icons.help),
+  ];
+
+  var drawerText = [
+    "勤怠時間入力",
+    "Primary",
+    "Social",
+    "Promotions",
+    "Starred",
+    "Snoozed",
+    "Important",
+    "Sent",
+    "Outbox",
+    "Drafts",
+    "All mail",
+    "Spam",
+    "Bin",
+    "[Imap]/Sent",
+    "[Imap]/Trash",
+    "Settings",
+    "Help & feedback"
+  ];
+  Text email = new Text(
+    "ko.rikujyou1114@gmail.com",
+    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15.0)
+  );
+  Text name = new Text(
+    "Kai Ouchi",
+    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15.0)
+  );
+  return Drawer(
+    child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.blueAccent),
+              accountName: name,
+              accountEmail: email,
+              currentAccountPicture: Icon(
+                Icons.account_circle,
+                size: 50.0,
+                color: Colors.white,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 0.0),
+                itemCount: drawerText.length,
+                itemBuilder: (context, position){
+                  return ListTile(
+                    leading: drawerIcons[position],
+                    title: Text(drawerText[position],
+                      style: TextStyle(fontSize: 15.0)
+                    ),
+                  );
+                }
+            ),)
+          ],
+        ),
+  );
 }
