192b24bbb60f282f7249ed0fddb652488faf3ec1
Kai Ouchi <ko.rikujyou1114@gmail.com>
2019-05-06 22:50:39 +0900

    phone button

--- lib/pages/call_phone_page.dart
+++ lib/pages/call_phone_page.dart
@@ -19,44 +19,96 @@ class _CallPhonePageState extends State<CallPhonePage> {
 Widget _callPhoneListGenerator() {
   return ListView.builder(
       itemCount: MailGenerator.mailListLength,
-      itemBuilder: ((context, position) {
+      itemBuilder: (context, position) {
         MailContent mailContent = MailGenerator.getMailContent(position);
-        return Column(
-          children: <Widget>[
-            Padding(
-                padding: EdgeInsets.only(
-                    left: 14.0, right: 14.0, top: 5.0, bottom: 5.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(
-                      Icons.account_circle,
-                      size: 55.0,
-                      color: Colors.lightBlue,
+        return Column(children: <Widget>[
+          Padding(
+            padding:
+                EdgeInsets.only(left: 14.0, right: 14.0, top: 5.0, bottom: 5.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.account_circle,
+                  size: 55.0,
+                  color: Colors.red,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              mailContent.sender,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black87,
+                                  fontSize: 17.0),
+                            ),
+                            // Text(
+                            //   mailContent.time,
+                            //   style: TextStyle(
+                            //       fontWeight: FontWeight.w400,
+                            //       color: Colors.black54,
+                            //       fontSize: 13.5),
+                            // ),
+                          ],
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  mailContent.subject,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.black54,
+                                      fontSize: 15.5),
+                                ),
+                                Text(
+                                  mailContent.message,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.black54,
+                                      fontSize: 15.5),
+                                )
+                              ],
+                            ),
+                            Ink(
+                                decoration: ShapeDecoration(
+                                    color: Colors.blue[300],
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10.0))),
+                                child: SizedBox(
+                                  width: 70.0,
+                                  height: 40.0,
+                                  child: IconButton(
+                                      // iconSize: 25.0,
+                                      icon: Icon(
+                                        Icons.phone,
+                                        size: 30.0,
+                                      ),
+                                      color: Colors.white,
+                                      onPressed: () {
+                                        print('pressed!');
+                                      }),
+                                ))
+                          ],
+                        ),
+                      ],
                     ),
-                    Expanded(
-                      child: Padding(
-                          padding: const EdgeInsets.only(left: 10.0),
-                          child: Column(
-                            children: <Widget>[
-                              Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: <Widget>[
-                                    Text(
-                                      mailContent.sender,
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w400,
-                                          color: Colors.black87,
-                                          fontSize: 17.0),
-                                    ),
-                                  ])
-                            ],
-                          )),
-                    ),
-                  ],
-                ))
-          ],
-        );
-      }));
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+        ]);
+      });
 }
--- lib/pages/mail_generator.dart
+++ lib/pages/mail_generator.dart
@@ -3,13 +3,13 @@ import 'mail_content.dart';
 
 class MailGenerator {
   static var mailList = [
-    MailContent("Happy Halloween", "John Doe", "31 Oct",
+    MailContent("Happy Halloween", "山田 貴之", "31 Oct",
         "This is a simple demo mail..."),
-    MailContent("Happy Halloween", "John Doe", "31 Oct",
+    MailContent("Happy Halloween", "劇団ひとり", "31 Oct",
         "This is a simple demo mail..."),
-    MailContent("Happy Halloween", "John Doe", "31 Oct",
+    MailContent("Happy Halloween", "黒木 瞳", "31 Oct",
         "This is a simple demo mail..."),
-    MailContent("Happy Halloween", "John Doe", "31 Oct",
+    MailContent("Happy Halloween", "橋下 環奈", "31 Oct",
         "This is a simple demo mail..."),
     MailContent("Happy Halloween", "John Doe", "31 Oct",
         "This is a simple demo mail..."),
